51e5fb069da0a54bb4aad67eabe691b96c670544
joao.cordeiro <joaocordeiro2134@gmail.com>
2024-03-11 11:55:44 -0300

    Add schema

---
+++ database/schema.sql
@@ -0,0 +1,18 @@
+use maco;
+
+create table user_lvl(
+	id int auto_increment primary key,
+    description varchar(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+create table user(
+	id int auto_increment primary key,
+    name varchar(255) not null,
+    cpf varchar(30) not null unique, 
+    email varchar(255) not null unique,
+    password varchar(255) not null,
+    lvl int not null,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    foreign key(lvl) REFERENCES user_lvl(id)    
+);
