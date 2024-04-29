4498ac5055dff6ba6478e2ff28900ce7a9a517e0
joao.cordeiro <joaocordeiro2134@gmail.com>
2024-04-29 17:37:02 -0300

    Nova opção de filtro e pagina de configurações

--- src/Helpers/ArticleHelper.php
+++ src/Helpers/ArticleHelper.php
@@ -14,36 +14,50 @@ class ArticleHelper
     public static function conditionByListByAdmin(object $params): string
     {
         switch($params) {                    
-            case isset($params->article_id):
+            // Só ID
+            case isset($params->article_id): 
                 return "article.id = {$params->article_id}";   
                 break;
-            case isset($params->article_title, $params->article_status):
-                return "article.title like '%{$params->article_title}%' and article.status = {$params->article_status}";
-                break;               
-            case isset($params->article_title):
-                return "article.title like '%{$params->article_title}%'";
-                break;
-            case isset($params->article_status, $params->course_id, $params->event_id):
-                return "article.status = {$params->article_status} and course.id = {$params->course_id} and article.event = {$params->event_id}";
-                break;
-            case isset($params->article_status, $params->course_id):
-                return "article.status = {$params->article_status} and course.id = {$params->course_id}";
-                break;
-            case isset($params->event_id, $params->course_id):
-                return "article.event = {$params->event_id} and course.id = {$params->course_id}";
-                break;
+            // Só status
             case isset($params->article_status):
                 return "article.status = {$params->article_status}";
                 break;
+            // Só titulo           
+            case isset($params->article_title):
+                return "article.title like '%{$params->article_title}%'";
+                break;
+            // Só evento
             case isset($params->event_id):
                 return "article.event = {$params->event_id}";
                 break;
+            // Só curso
             case isset($params->course_id):
                 return "course.id = {$params->course_id}";
                 break;
+            // Só nome do curso
             case isset($params->course_name):
                 return "course.name like '%{$params->course_name}%'";                
                 break;
+            // Titulo e status
+            case isset($params->article_title, $params->article_status): 
+                return "article.title like '%{$params->article_title}%' and article.status = {$params->article_status}";
+                break;               
+            // Status, curso e evento
+            case isset($params->article_status, $params->course_id, $params->event_id):
+                return "article.status = {$params->article_status} and course.id = {$params->course_id} and article.event = {$params->event_id}";
+                break;
+            // Status e curso
+            case isset($params->article_status, $params->course_id):
+                return "article.status = {$params->article_status} and course.id = {$params->course_id}";
+                break;
+            // Evento e status
+            case isset($params->event_id, $params->article_status):
+                return "article.event = {$params->event_id} and article.status = {$params->article_status}";
+                break;
+            // Evento e curso
+            case isset($params->event_id, $params->course_id):
+                return "article.event = {$params->event_id} and course.id = {$params->course_id}";
+                break;                    
         }        
         return '';       
     }    
