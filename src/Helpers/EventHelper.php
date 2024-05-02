edee3a53fb492bd825298c44f3e925ce4aa79d21
joao.cordeiro <joaocordeiro2134@gmail.com>
2024-05-02 17:10:04 -0300

    Add filtro no evento

--- src/Helpers/EventHelper.php
+++ src/Helpers/EventHelper.php
@@ -11,8 +11,14 @@ class EventHelper
      */
     public static function conditionByList(object $params): string
     {
+        if (isset($params->event_id, $params->status)) {
+            return "id = {$params->event_id} and status = {$params->status}";
+        }
         if (isset($params->status)) {
             return "status = {$params->status}";
+        }
+        if (isset($params->event_id)) {
+            return "id = {$params->event_id}";
         }
         return '';       
     }         
