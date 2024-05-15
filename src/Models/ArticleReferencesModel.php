d2c37dc6e62df6b9b30581b3ba5cab1955fd6d68
joao.cordeiro <joao.victor@nubbi.com.br>
2024-05-14 22:32:17 -0300

    add model

---
+++ src/Models/ArticleReferencesModel.php
@@ -0,0 +1,38 @@
+<?php 
+
+namespace MacoBackend\Models;
+
+use SimpleDB\Opers;
+
+class ArticleReferencesModel extends Opers {
+
+    private $table = 'article_references';
+    private $columns = [               
+        'article:int:not null',       
+        'reference:text:not null',            
+    ];
+
+    public function __construct() {        
+        parent::__construct($this->table, $this->columns, false);
+    }   
+
+    public function getID() 
+    {
+        return parent::result()->id;
+    }    
+
+    public function getArticle() 
+    {
+        return parent::result()->article;
+    }   
+
+    public function getReference() 
+    {
+        return parent::result()->reference;
+    }    
+
+    public function getCreatedAt() 
+    {
+        return parent::result()->created_at;
+    }
+}
